bf156825d8b428e81b4aa1b2e22cefdfb1b65f70
Samuel Niang <samuel.niang@gmail.com>
2025-12-08 20:04:49 +0100

    Refactor error handling in TimeStampSelector component to set error directly from the caught exception

--- src/TimeStampSelector.jsx
+++ src/TimeStampSelector.jsx
@@ -3,7 +3,6 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import e from "express";
 
 /**
  * TimeStampSelector is a React component that renders a dropdown menu for selecting
@@ -71,10 +70,7 @@ function TimeStampSelector({
         setJsonFiles(filteredData); // Update available files
         setSelectedFile(filteredData[0]); // Set default selected file
       } catch (error) {
-        const errorMessage = `[ERROR] ${error.message}
-            TimeStampSelector failed to fetch JSON files`;
-        console.error(errorMessage);
-        setError(errorMessage); // Set error if fetch fails
+        setError(error); // Set error if fetch fails
       }
     };
     fetchFiles();
