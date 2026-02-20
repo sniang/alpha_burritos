744311ddd5ad8c0b865d2c1107f77138afc2debd
Samuel Niang <samuel.niang@gmail.com>
2026-02-20 11:33:30 +0100

    Enhance error message in TimeStampSelector to provide detailed temporary fix instructions and reference ReadMe for a permanent solution

--- src/TimeStampSelector.jsx
+++ src/TimeStampSelector.jsx
@@ -73,7 +73,11 @@ function TimeStampSelector({
         const enhancedError = new Error(`TimeStampSelector failed to fetch JSON files
             Probably cause: EOS is not correctly mounted or there is no Kerberos ticket`);
         enhancedError.cause = error;
-        enhancedError.bashCode = `# Temporary fix\n~/Desktop/burrito_src/mount_eos_for_7_days.sh\n #Check ReadMe for more details and a permanent solution.`;
+        enhancedError.bashCode = `# Temporary fix\n~/Desktop/burrito_src/mount_eos_for_7_days.sh`;
+        enhancedError.bashCode += `\n#Check ReadMe for more details and a permanent solution. The following should solve permanently the issue:\n`;
+        enhancedError.bashCode += `#1. Create a Kerberos ticket with kinit or wait for the watchdog to do it\n#2. Mount EOS with the command below:\n`;
+        enhancedError.bashCode += `sudo eosxd -ofsname=eos-burritos-data`;
+
         console.error('[ERROR]', enhancedError.message);
         setError(enhancedError); // Set error if fetch fails
       }
